29203a049eb26637d90769ca172f73275badcdad
shubham parmar <122009457+Kaiser7272@users.noreply.github.com>
2023-01-24 23:45:34 +0530

    1ST

---
+++ ccc.js
@@ -0,0 +1,12 @@
+let calAVG = (a, b, c) => (a + b + c) / 3;
+const dolphinSAVG = calAVG(44, 23, 71);
+let koalaAVG = calAVG(65, 54, 49);
+
+
+const checkWinner = function (SCORE1,SCORE2) {     if (dolphinSAVG > koalaAVG {console.log(`that dolphins won`)
+}else if (koalaAVG > dolphinSAVG) {
+    console.log(`that koalas won`);
+}
+    
+}
+const OURWINNER = checkWinner(dolphinSAVG,koalaAVG);
